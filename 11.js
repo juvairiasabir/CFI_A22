3d92967367930958c47b25f1c19833103da08e8d
Juvairia Sabir <juvairiasabir@DellInspiron>
2022-08-01 09:15:49 +0530

    updates

---
+++ 11.js
@@ -0,0 +1,38 @@
+// //
+// for (i=1; i<=10; i++){
+
+//     if (i==6) { 
+//         // break;
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// break - stops next iterations
+// continue - skips current iterations
+
+// let i=1
+// while (i<=10){
+//     console.log(i)
+//     if (i==5){
+//         i++
+//         continue;
+
+//     }
+//     // console.log(i)
+//     i++
+// }
+
+//Functions
+
+//SYNTAX
+
+//Function declaration
+
+// function_name(arguments) // function call back
+//FUNCTION WITHOUT PARAMETERS
+// function add(){
+//     console.log(2+3)
+// }
+
+// add()
