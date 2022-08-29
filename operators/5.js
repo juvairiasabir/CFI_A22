08c62b33063bc54b21a4b9d5f64eea008cc1b738
Juvairia Sabir <juvairiasabir@DellInspiron>
2022-08-29 11:03:19 +0530

    done with operators

---
+++ operators/5.js
@@ -0,0 +1,63 @@
+// Operators
+// 1. Arithematic operator : +,-,*,/,%,++,--,**
+// (Mod, Modulo, Modulus) 
+// 2. Assignment operator : =, +=, -=, !=, >=, <=, *=,
+// 3. Comparison operator : >,<, <=, >=, !=, ==, ===, !==
+// 4. Logical operator : &&, ||, !
+// 5. Bitiwise operator : &, |, ~, ^, <<, >>
+// 6. Relational operator : in
+// 7. String operator : +
+// 8. Ternary operator : ?:
+
+
+// 1. Arithmetic operator : +,-,*,/,%,++,--,**
+
+a = 2,b = 4
+
+// console.log(a+b)
+// console.log(a+6)
+// console.log(9+b)
+//console.log()
+
+
+
+// console.log(a*b+5)
+// console.log(3+a*b)
+// console.log(a/b/0)
+// console.log(a*b*0)
+// console.log(a*b+a/2-2)
+// console.log(a*(b+a)/2-2)
+// console.log(a+(a-(b*a)))
+
+// a = 2,b = 4
+// console.log(a%b)
+// // divisor = b ; dividend = a // remainder
+// // divisor>dividend
+
+
+// console.log(Math.floor(a/b))
+// console.log(Math.ceil(0.9))
+// // ceil,floor
+// // console.log(Math.abs(-0.9))
+// console.log(Math.round(0.9))
+// console.log(Math.round(0.1))
+// console.log(Math.round(0.5))
+// console.log(Math.round(0.6))
+// console.log(Math.round(0.3))
+
+// console.log(4**2)
+// console.log(4**4)
+
+// console.log(8**2**2)
+// console.log(8**(2**2))
+// console.log((8**)2**2))
+
+var a=4, b=4, c=3
+console.log(a**b)
+
+
+console.log(a**b**c+b/a%c)
+
+
+
+
---
+++ operators/8.js
@@ -0,0 +1,89 @@
+// Bitwise operators
+
+// var a=7,b=5
+// console.log(a&b)
+
+// var a=17,b=15
+// console.log(a&b)
+
+// var a=40,b=25
+// console.log(a&b)
+
+// var a=117,b=3
+// console.log(a>>b)
+
+
+//Binary - 0bnnnn (2)
+// Decimal - nnn (10)
+// Octal - 0onnn (8)
+//Hexadeccimal - 0xnnn(16)
+
+// toString 
+// var a = 1000
+
+// console.log(a.toString(10)) // To convert to decimal
+// console.log(a.toString(2)) // To convert to BINARY
+// console.log(a.toString(8)) // To convert to OCTAL
+// console.log(a.toString(16)) // To convert to Hexa-decimal
+
+//unary operator
+// +,-
+
+// console.log(1)
+// console.log(1)
+
+// var a = 1, b="1" ,c=true, d="true"
+
+// console.log(a, typeof a)
+// console.log(b, typeof b)
+// console.log(c, typeof c)
+// console.log(d, typeof d)
+
+// console.log(+a, typeof +a)
+// console.log(+b, typeof +b)
+// console.log(+c, typeof +c)
+// console.log(+d, typeof +d)
+
+// console.log(-a, typeof -a)
+// console.log(-b, typeof -b)
+// console.log(-c, typeof -c)
+// console.log(-d, typeof -d)
+
+// console.log(1+1+1+1)
+
+// console.log(1+'1')
+
+// console.log('1'+1+1+1)
+
+// console.log(1+'1'+1+1)
+
+// console.log(1+1+'1'+1)
+
+// console.log(1+1+1+'1')
+
+
+
+// console.log('1' - '2')
+// console.log('1' - 2)
+// console.log(1 - '2')
+// console.log(100 - 1 + '2')
+// console.log('100' - 1 - '2')
+// console.log('100' - 1 - 2)
+
+
+
+// console.log('50' - 1 - 2)
+
+// console.log('1' * '2')
+// console.log('1' * 2)
+// console.log(1 * '2')
+// console.log(100 * 1 * '2')
+// console.log('100' * 1 + '2')
+// console.log('100' * 1 * 2)
+// console.log("100" * "2")
+
+// console.log('100' + null)
+// console.log(100 + null)
+// console.log(8 * null)
+
+
